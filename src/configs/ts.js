2e4af8c30637a8e7c89cbaefaae16ef1cc01908a
RedGuy12 <paul@reid-family.org>
2021-12-25 13:00:55 -0600

    3.0.0-beta.10

--- src/configs/ts.js
+++ src/configs/ts.js
@@ -4,15 +4,16 @@
 module.exports = {
 
 	"extends": [
+		"plugin:@redguy12/esm",
 		"hardcore/ts",
-		require("path").resolve(__dirname, "./_recommended.js"),
-		"plugin:@redguy12/esm"
 	],
 	"rules": {
+		...require("./recommended.js").rules,
 		"no-tabs":0,
 		"jsdoc/require-jsdoc": 0,
 		"@typescript-eslint/no-namespace": 0,
 		"@typescript-eslint/consistent-type-definitions": 0,
 		"etc/prefer-interface": 0,
-		"no-duplicate-imports": 0,}
+		"no-duplicate-imports": 0,
+	}
 }
